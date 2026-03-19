8b4699216a61b2fcbef1e934123cfd68c12b3dfb
Eric-Hsia <ada_inc@126.com>
2026-03-19 17:16:21 +0800

    Add prototype for lsm6dsox_self_test function in main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -53,7 +53,7 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-
+void lsm6dsox_self_test(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
